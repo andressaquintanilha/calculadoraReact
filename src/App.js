4f07dcf0346905a334a413b5a7f656b3c80984ff
andressaquintanilha.email <quintanilhalg3@gmail.com>
2022-02-21 12:11:28 -0300

    calculadora finalizada

--- src/App.js
+++ src/App.js
@@ -1,68 +1,113 @@
 import React, {Component} from "react";
+import './App.css'
+import logo from "./logo.png"
 
 class Calculadora extends Component {
   
   state = {
     n1: "",
     n2: "",
-    result: ""
+    res: ""
   }
 
-    handleChange = (event) => {
+    handleChange1 = (event) => {
       this.setState({
-        n1: event.target.value
+        n1: Number(event.target.value)
         //irá pegar o valor recebido no input
       })
     } 
 
-    handleChangeTwo = (event) => {
+    handleChange2 = (event) => {
       this.setState({
-        n2: event.target.value
+        n2: Number(event.target.value)
         //irá pegar o valor recebido no input
       })
     } 
-    //console.log(event.target.value)
-
-    mult = () => {
-      const {n1, n2} = this.state
-      this.setState({
-        result: n1 * n2
-      })
-    }
 
     div = () => {
       const {n1, n2} = this.state
-      this.setState({
-        result: n1 / n2
-      })
+      if (n1 && n2 !== ""){
+        this.setState({
+          res: n1 / n2
+        })
+      } else {
+        this.setState({
+          res: "Nenhum valor foi digitado."
+        })
+      }
     }
 
-    som = () => {
+    mult = () => {
       const {n1, n2} = this.state
-      this.setState({
-        result: n1 + n2
-      })
+      if (n1 && n2 !== ""){
+        this.setState({
+          res: n1 * n2
+        })
+      } else {
+        this.setState({
+          res: "Nenhum valor foi digitado."
+        })
+      }
     }
 
     sub = () => {
       const {n1, n2} = this.state
-      this.setState({
-        result: n1 - n2
-      })
+      if (n1 && n2 !== ""){
+        this.setState({
+          res: n1 - n2
+        })
+      } else {
+        this.setState({
+          res: "Nenhum valor foi digitado."
+        })
+      }
+    }
+
+    som = () => {
+      const {n1, n2} = this.state
+      if (n1 && n2 !== ""){
+        this.setState({
+          res: n1 + n2
+        })
+      } else {
+        this.setState({
+          res: "Nenhum valor foi digitado."
+        })
+      }
+    }
+
+    clear = () => {
+      const {n1, n2} = this.state
+      if (n1 && n2 !== ""){
+        this.setState({
+          res: null,
+          n1: 0,
+          n2: 0
+        })
+      } else {
+        this.setState({
+          res: "Nenhum valor foi digitado."
+        })
+      }
     }
 
   render(){
     return(
-      <div>
+      <div className="container">
+        <div className="box">
+        <img src={logo}/>
         <h1>React App Calc</h1>
-        <h2>{this.state.result}</h2>
-        <input type="number" onChange={this.handleChange}/>
-        <input type="number" onChange={this.handleChangeTwo}/>
-          <button onClick={this.som}>+</button>
-          <button onClick={this.sub}>-</button>
-          <button onClick={this.mult}>*</button>
+        <h2>{this.state.res}</h2>
+        <input value={this.state.n1} type="number" onChange={this.handleChange1}/>
+        <input value={this.state.n2} type="number" onChange={this.handleChange2}/>
+        <div className="btns">
           <button onClick={this.div}>/</button>
-          <button>Clear</button>
+          <button onClick={this.mult}>*</button>
+          <button onClick={this.sub}>-</button>
+          <button onClick={this.som}>+</button>
+          <button onClick={this.clear}>Clear</button>
+        </div>
+          </div>
       </div>
     )
   }
